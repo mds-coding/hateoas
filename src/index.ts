f03176bdd26883645ac197fab35dfa930c9e966e
mdos-san <mdossan@protonmail.com>
2024-11-08 20:04:05 +0100

    feat: export all sub-packages

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "@mds-coding/hateoas-action";
+export * from "@mds-coding/hateoas-entity";
+export * from "@mds-coding/hateoas-field";
+export * from "@mds-coding/hateoas-link";
+export * from "@mds-coding/hateoas-resource";
